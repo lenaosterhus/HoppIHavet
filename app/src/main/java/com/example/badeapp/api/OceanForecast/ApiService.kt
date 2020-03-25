443c30c482e1cc3d6fc161e0ddf8a9ff151bc79e
miro <miroad@student.matnat.uio.no>
2020-03-25 22:14:39 +0100

    Pushing code

--- app/src/main/java/com/example/badeapp/api/OceanForecast/ApiService.kt
+++ app/src/main/java/com/example/badeapp/api/OceanForecast/ApiService.kt
@@ -9,6 +9,8 @@ internal interface ApiService {
 
     // https://in2000-apiproxy.ifi.uio.no/weatherapi/oceanforecast/0.9/.json?lat=59.9016&lon=10.665422
     // Suspend fun --> called in coroutines
+
+    //@Headers("User-Agent: ") @TODO add user agent
     @GET(".json")
     suspend fun getData(
         @Query("lat") lat: String,
